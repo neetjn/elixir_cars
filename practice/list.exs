884a84d6d5c34fdbd362a4d06a219d76858dd0f1
John Nolette <john.nolette@messagebird.com>
2022-10-29 19:00:40 -0400

    Working out list practice

--- practice/list.exs
+++ practice/list.exs
@@ -1,6 +1,21 @@
 defmodule ListPractice do
   def main do
-
+    lsOne = [1, 2, 3]
+    lsTwo = [4, 5, 6]
+    lsThree = lsOne ++ lsTwo
+    strLs = Enum.map(lsThree, fn num -> Integer.to_string(num) end)
+    IO.puts "Str list: #{strLs}"
+    combined = [1, 3, 5 | [7, 9]]
+    IO.inspect combined
+    base = [1, 2, 3, 4, 5, 6, 7, 8]
+    div2 = Enum.filter(base, fn num -> rem(num, 2) == 0 end)
+    strdiv = Enum.map(div2, fn num -> "#{Integer.to_string(num)}," end)
+    IO.puts strdiv
+    :apple = "Apple"
+    :orange = "Orange"
+    :pizza = "Pizza"
+    itemCosts = [[:apple, 1.0], [:orange, 0.75], [:pizza, 10.0]]
+    # item_map = List.foldl()
   end
 end
 
